b6d4c27ddab924b7140eacdd846fc78060449fb4
Pierre Michel <michel.pierre.35@gmail.com>
2015-12-04 11:54:41 +0100

    Implémentation de la fixation des cylindres dans les trous. TODO: Créer des préfabs.

---
+++ InteractionEtMultimodalite/Assets/Scripts/CylinderBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CylinderBehaviour : MonoBehaviour {
+
+	[HideInInspector]
+	public HoleBehaviour hole = null;
+	
+	private GameObject skin;
+
+	// Use this for initialization
+	void Awake() {
+		skin = transform.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if(hole == null) {
+			skin.transform.position = transform.position;
+			skin.transform.rotation = transform.rotation;
+		} else {
+			skin.transform.position = hole.transform.position;
+			skin.transform.rotation = hole.transform.rotation;
+		}
+	}
+}
---
+++ InteractionEtMultimodalite/Assets/Scripts/HoleBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class HoleBehaviour : MonoBehaviour {
+
+	private CylinderBehaviour occupator = null;
+
+	void OnTriggerEnter(Collider other) {
+		//If the other collider is a cylinder, ...
+		if(other.gameObject.GetComponent<CylinderBehaviour>() != null) {
+			//If the hole is not occupied, ...
+			if(occupator == null) {
+				//The hole becomes occupied.
+				occupator = other.gameObject.GetComponent<CylinderBehaviour>();
+				//Bind position.
+				occupator.hole = this;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		//If the other is the occupator, ...
+		if(occupator != null && occupator != other.gameObject.GetComponent<CylinderBehaviour>()) {
+			//Unbind position.
+			occupator.hole = null;
+			//The hole no longer is occupied.
+			occupator = null;
+		}
+	}
+
+}
