f8478110d5ea808b9dc293b3370dc3810372e8e6
Pierre Michel <michel.pierre.35@gmail.com>
2015-12-04 10:47:13 +0100

    Ajout de limites de tous les côtés + Reset de la scene avec 'R'.

---
+++ InteractionEtMultimodalite/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPosition : MonoBehaviour {
+
+	private Vector3 startPosition;
+	private Quaternion startRotation;
+
+	void Start () {
+		startPosition = transform.position;
+		startRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R)) {
+			transform.position = startPosition;
+			transform.rotation = startRotation;
+		}
+	}
+}
